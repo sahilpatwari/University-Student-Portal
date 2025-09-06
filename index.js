876f582b9723b68cf8fe58f848fedfea8dc9b027
Sahil <sahilpatwari29@gmail.com>
2025-09-06 21:56:22 +0530

    Certain fixes

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import mysql from "mysql2/promise";
 import bodyParser from "body-parser";
+import env from "dotenv";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+env.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "student_details",
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
 });
 pool.getConnection((err, connection) => {
   if (err) {
